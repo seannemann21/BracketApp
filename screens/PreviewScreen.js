919f263fe1ce7ebf0fc5236b3308cde6bd96bfc6
Sean Nemann <seannemann@beams-mbp.home>
2019-10-16 23:18:41 -0400

    Added button to join brackets

--- screens/PreviewScreen.js
+++ screens/PreviewScreen.js
@@ -4,8 +4,10 @@ import axios from 'axios';
 import Bracket from '../components/Bracket';
 
 export default function SearchScreen(props) {
+  const user_id = 1;
   const id = props.navigation.getParam('id');
   const [bracket, setBracket] = useState();
+  const [joined, setJoined] = useState(false);
 
   const getBracket = () => {
     axios
@@ -16,8 +18,27 @@ export default function SearchScreen(props) {
       .then(resp => {
         console.log(resp.data);
         setBracket(resp.data);
+        if (resp.data.users.find(u => u.id === user_id)) {
+          setJoined(true);
+        }
       })
       .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const join = () => {
+    axios
+      .create({
+        baseURL: 'http://10.0.2.2:3000/',
+      })
+      .post(`/users/${user_id}/brackets`, {
+        bracket_id: id,
+      })
+      .then(() => {
+        setJoined(true);
+      })
+      .catch(function(error) {
         console.log(error);
       });
   };
@@ -31,8 +52,16 @@ export default function SearchScreen(props) {
       style={{
         height: '100%',
       }}>
-      {bracket && <Bracket bracket={bracket} />}
-      <Button title="Preview" />
+      {bracket && (
+        <>
+          <Bracket bracket={bracket} />
+          {joined ? (
+            <Text>Subscribed</Text>
+          ) : (
+            <Button title="Join" onPress={join} />
+          )}
+        </>
+      )}
     </View>
   );
 }
